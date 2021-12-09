c1dc6ef59c27bc8f32ab56e40650e47ca744fa51
teradaian <teradaian@gmail.com>
2021-12-09 08:34:20 -0800

    attempt balancing

--- src/Components/BurgerShop/IngredientList.jsx
+++ src/Components/BurgerShop/IngredientList.jsx
@@ -5,15 +5,17 @@ const IngredientList = (props) => {
 	const [hasBuns, setHasBuns] = useState(false)
 
 	useEffect(() => {
-		setHasBuns(props.stack.filter(el => el.type === 'bun').length === 2)
+		setHasBuns(props.stack.filter(el => el.type === 'bun'))
 	}, [props.stack])
 
-	const ingredientStack = props.ingredients.map((ingredient, idx) => {
-		if (hasBuns && ingredient.type === 'bun') {
-			return <Ingredient list={true} disabled={true} key={idx} ingredient={ingredient} addToBurger={props.addToBurger} />
-		}
-		return <Ingredient list={true} key={idx} ingredient={ingredient} addToBurger={props.addToBurger} />
-	})
+	const ingredientStack = props.ingredients.map((ingredient, idx) => (
+		hasBuns && ingredient.type === 'bun' ? 
+		<Ingredient list={true} disabled={true} key={idx} ingredient={ingredient} addToBurger={props.addToBurger} />
+		:
+		<Ingredient list={true} key={idx} ingredient={ingredient} addToBurger={props.addToBurger} />
+	))
+
+	const balancedBuns = ingredientStack()
 	
 	return (
 		<ul>
