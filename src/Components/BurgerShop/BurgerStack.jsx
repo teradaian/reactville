1100017f543d7f08784d0a280c699dcdc0c61237
teradaian <teradaian@gmail.com>
2021-12-09 10:40:59 -0800

    fix prop issue in Burger stack

--- src/Components/BurgerShop/BurgerStack.jsx
+++ src/Components/BurgerShop/BurgerStack.jsx
@@ -1,22 +1,22 @@
 import React from "react"
 import Ingredient from "./Ingredient"
 
-const BurgerStack = ({ ingredients, hasBuns, removeBuns, removeFromBurger }) => {
+const BurgerStack = (props) => {
 
-  const bun = hasBuns ? ingredients.filter(el => el.type === 'bun').map((ingredient, idx) => (
-    <Ingredient key={`bun-${idx}`} ingredient={ingredient} removeFromBurger={removeBuns} />
+  const bun = props.hasBuns ? props.ingredients.filter(el => el.type === 'bun').map((ingredient, idx) => (
+    <Ingredient key={`bun-${idx}`} ingredient={ingredient} removeFromBurger={props.removeBuns} />
   )) : ""
 
-  const fillings = ingredients.map((ingredient, idx) => (
+  const fillings = props.ingredients.map((ingredient, idx) => (
     ingredient.type !== 'bun' &&
-    <Ingredient key={idx} idx={idx} ingredient={ingredient} removeFromBurger={removeFromBurger} />
+    <Ingredient key={idx} idx={idx} ingredient={ingredient} removeFromBurger={props.removeFromBurger} />
   )).reverse()
 
   const balancedBurger = [bun, ...fillings, bun]
 
   return (
     <ul className="burger-stack">
-      {ingredients.length ?
+      {props.ingredients.length ?
         balancedBurger
         :
         <div>
